258c0c80e36056ff4e82a1d7e58fac794f68a1aa
Arjav Vakharia <vakhariaarjav97@gmail.com>
2021-04-26 17:41:23 -0500

    Final commit

---
+++ src/app/product-service/product.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Observable , of } from 'rxjs';
+//import { Product } from '../product';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ProductService {
+
+  constructor() { }
+
+ }
---
+++ src/app/products.ts
@@ -0,0 +1,22 @@
+import { Product } from './product';
+
+export const products : Product[] = [
+      {
+        id: 1,
+        name: 'Phone XL',
+        price: 799,
+        desc: 'A large phone with one of the best screens'
+      },
+      {
+        id: 2,
+        name: 'Phone Mini',
+        price: 699,
+        desc: 'A great phone with one of the best cameras'
+      },
+      {
+        id: 3,
+        name: 'Phone Standard',
+        price: 299,
+        desc: 'Good one'
+      }
+]
